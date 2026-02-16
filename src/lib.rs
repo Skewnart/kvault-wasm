6a1a867c1d64ddad142f1b6e1b2577db6d83f391
skewnart <corentinz@outlook.fr>
2026-02-16 23:59:06 +0100

    feat: adding kyber test

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,22 @@ extern "C" {
 pub fn greet(name: &str) {
     alert(&format!("Hello, {name}!"));
 }
+
+#[wasm_bindgen]
+pub fn generate_keypair() {
+    let mut rng = rand::thread_rng();
+
+    let keys = pqc_kyber::keypair(&mut rng).unwrap();
+    println!("public : {:?}", keys.public);
+    println!("secret : {:?}", keys.secret);
+}
+#[cfg(test)]
+mod tests {
+    use crate::{generate_keypair, test_all_steps};
+
+    #[test]
+    fn test_generate_keypair() {
+        generate_keypair();
+    }
+}
+
