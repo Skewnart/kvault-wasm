64f4104b5a12e59fffa54dd5cdb92d3e33631048
skewnart <corentinz@outlook.fr>
2026-02-19 00:09:18 +0100

    feat: read entry

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 mod models;
 
-use aes_gcm::{AeadCore, Aes256Gcm, KeyInit};
+use aes_gcm::{AeadCore, Aes256Gcm, KeyInit, Nonce};
 use aes_gcm::aead::{Aead};
 use argon2::{Argon2};
 use rand::rngs::OsRng;
@@ -19,9 +19,9 @@ extern "C" {
 // }
 
 #[wasm_bindgen]
-pub fn generate_register_envelope(master_password: String, user_email: String) -> Result<RegisterEnvelope, JsValue> {
+pub fn generate_register_envelope(master_password: String, user_unique: String) -> Result<RegisterEnvelope, JsValue> {
     let master = master_password.as_bytes();
-    let salt = user_email.as_bytes();
+    let salt = user_unique.as_bytes();
 
     let mut k_master = [0u8; 32];
     Argon2::default().hash_password_into(master, salt, &mut k_master).map_err(|e| JsValue::from(e.to_string()))?;
@@ -56,6 +56,27 @@ pub fn create_entry(password: String, pk: Vec<u8>) -> Result<Entry, JsValue> {
         enc_kyber.to_vec(),
         pwd_nonce.as_slice().into()
     ))
+}
+
+#[wasm_bindgen]
+pub fn read_entry(master_password: String, user_unique: String, enc_sk: Vec<u8>, sk_nonce: Vec<u8>, enc_pwd: Vec<u8>, enc_kyber: Vec<u8>, pwd_nonce: Vec<u8>) -> Result<String, JsValue> {
+    let master_password = master_password.as_bytes();
+    let salt = user_unique.as_bytes();
+
+    let mut k_master = [0u8; 32];
+    Argon2::default().hash_password_into(master_password, salt, &mut k_master).map_err(|e| JsValue::from(e.to_string()))?;
+
+    let cipher = Aes256Gcm::new_from_slice(&k_master).map_err(|e| JsValue::from(e.to_string()))?;
+    let sk_nonce = Nonce::from_slice(sk_nonce.as_slice());
+    let sk = cipher.decrypt(&sk_nonce, enc_sk.as_ref()).map_err(|e| JsValue::from(e.to_string()))?;
+
+    let cipher_key = pqc_kyber::decapsulate(&enc_kyber, &sk).map_err(|e| JsValue::from(e.to_string()))?;
+
+    let cipher = Aes256Gcm::new_from_slice(&cipher_key).map_err(|e| JsValue::from(e.to_string()))?;
+    let pwd_nonce = Nonce::from_slice(pwd_nonce.as_slice());
+    let pwd = cipher.decrypt(&pwd_nonce, enc_pwd.as_ref()).map_err(|e| JsValue::from(e.to_string()))?;
+
+    Ok(String::from_utf8(pwd).map_err(|e| JsValue::from(e.to_string()))?)
 }
 
 #[wasm_bindgen]
