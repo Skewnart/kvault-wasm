39791355abf84aa72386456cfdbf66b58bd6f163
skewnart <corentinz@outlook.fr>
2026-02-19 23:18:12 +0100

    feat: remove full process

--- src/lib.rs
+++ src/lib.rs
@@ -9,16 +9,6 @@ use crate::models::entry::Entry;
 use crate::models::register_envelope::RegisterEnvelope;
 
 #[wasm_bindgen]
-extern "C" {
-    // fn alert(s: &str);
-}
-
-// #[wasm_bindgen]
-// pub fn greet(name: &str) {
-//     alert(&format!("Hello, {name}!"));
-// }
-
-#[wasm_bindgen]
 pub fn generate_register_envelope(master_password: String, user_unique: String) -> Result<RegisterEnvelope, JsValue> {
     let master = master_password.as_bytes();
     let salt = user_unique.as_bytes();
@@ -79,81 +69,12 @@ pub fn read_entry(master_password: String, user_unique: String, enc_sk: Vec<u8>,
     Ok(String::from_utf8(pwd).map_err(|e| JsValue::from(e.to_string()))?)
 }
 
-#[wasm_bindgen]
-pub fn test_all_steps(password: String) -> Result<String, JsValue> {
-    // Register
-    println!("# REGISTER");
-    let master = b"MasterPassword";
-    println!("master : {:?}", master);
-
-    let mut k_master = [0u8; 32];
-    let salt = b"email_user";
-    Argon2::default().hash_password_into(master, salt, &mut k_master).unwrap();
-    println!("enc_master : {:?}", k_master);
-
-    let mut rng = rand::thread_rng();
-    let kyber_keys = pqc_kyber::keypair(&mut rng).unwrap();
-    println!("public kyber : {:?}", kyber_keys.public);
-    println!("secret kyber : {:?}", kyber_keys.secret);
-
-    let cipher = Aes256Gcm::new_from_slice(&k_master).unwrap();
-    let nonce1 = Aes256Gcm::generate_nonce(&mut OsRng);
-    let enc_sk = cipher.encrypt(&nonce1, kyber_keys.secret.as_ref()).unwrap();
-
-    println!("enc_sk: {:?}", enc_sk);
-    //to send : public kyber + enc_sk
-
-    // Création Entry
-    println!("# CREATION ENTRY");
-    // recoit public kyber
-    let pwd = password.as_bytes();
-    println!("pwd : {:?}", pwd);
-
-    let mut rng = rand::thread_rng();
-    let (enc_kyber, cipher_key) = pqc_kyber::encapsulate(&kyber_keys.public, &mut rng).unwrap();
-
-    let cipher = Aes256Gcm::new_from_slice(&cipher_key).unwrap();
-    let nonce2 = Aes256Gcm::generate_nonce(&mut OsRng);
-    let enc_pwd = cipher.encrypt(&nonce2, pwd.as_ref()).unwrap();
-
-    println!("cipher_key: {:?}", cipher_key);
-    println!("enc_pwd: {:?}", enc_pwd);
-    println!("enc_kyber: {:?}", enc_kyber);
-    //to send : entry_metadata, enc_kyber + enc_pwd
-
-    // LECTURE ENTRY
-    println!("# LECTURE ENTRY");
-    // Reception de entry_metadata, enc_pwd, enc_kyber, enc_sk
-
-    let mut k_master = [0u8; 32];
-    let salt = b"email_user";
-    Argon2::default().hash_password_into(master, salt, &mut k_master).unwrap();
-    println!("k_master : {:?}", k_master);
-
-    let cipher = Aes256Gcm::new_from_slice(&k_master).unwrap();
-    let sk = cipher.decrypt(&nonce1, enc_sk.as_ref()).unwrap();
-    println!("sk: {:?}", sk);
-
-    let cipher_key = pqc_kyber::decapsulate(&enc_kyber, &sk).unwrap();
-    println!("cipher_key: {:?}", cipher_key);
-
-    let cipher = Aes256Gcm::new_from_slice(&cipher_key).unwrap();
-    let pwd = cipher.decrypt(&nonce2, enc_pwd.as_ref()).unwrap();
-    println!("pwd: {:?}", pwd);
-
-    String::from_utf8(pwd).map_err(|_|JsValue::NULL)
-}
-
 #[cfg(test)]
 mod tests {
-    use crate::{test_all_steps};
-
     #[test]
-    fn test_test_all_steps() {
-        let initial_password = "mot de passe";
-        let pwd = test_all_steps(String::from(initial_password)).unwrap();
+    fn test_step() {
 
-        assert_eq!(pwd, initial_password);
+        // assert_eq!();
     }
 }
 
